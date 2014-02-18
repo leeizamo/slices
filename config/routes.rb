87674bba6568d54a157d9cd5b9fef8038d60acfd
Amour Jaxon <leeizamo@gmail.com>
2014-02-18 04:40:39 -0500

    update to pages with colors, added a nav bars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Slices::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
-
+  get "About" => "pages#about"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
